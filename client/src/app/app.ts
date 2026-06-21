ac67dfff4aed63ebb3bc26bd15d55784b8aa8a8e
itsJohnDev <johncarloaguilar.pinlac@gmail.com>
2026-06-21 16:55:11 +0800

    Added basic auth

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,22 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Nav } from '../layout/nav/nav';
+import { AccountService } from '../core/services/account-service';
+import { Home } from '../features/home/home';
+import { User } from '../types/users';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'client';
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
+    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
       next: (response) => this.members.set(response),
       error: (error) => console.log(error),
       complete: () => console.log('Completed the http request'),
     });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 }
---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,34 @@
+import { Component, inject, input, output } from '@angular/core';
+import { RegisterCreds, User } from '../../../types/users';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../../core/services/account-service';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  private accountService = inject(AccountService);
+  cancelRegister = output<boolean>();
+  protected creds: RegisterCreds = {
+    email: '',
+    displayName: '',
+    password: '',
+  };
+
+  register() {
+    this.accountService.register(this.creds).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
